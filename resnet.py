6077c9ca3f7271b22a18e477dd4088d76a4ebd4b
jonahanton <jonahlouisanton@gmail.com>
2022-01-30 20:15:20 +0000

    Added squeeze-excitation resnets

--- resnet.py
+++ resnet.py
@@ -1,7 +1,11 @@
 """
+
 Implementations of:
-i. ResNet18 - arXiv:1512.03385
-ii. ResNet18C/D - arXiv:1812.01187
+
+    i. ResNet18 - arXiv:1512.03385
+    ii. ResNet18C/D - arXiv:1812.01187
+    iii. SE-ResNet18 (Squeeze-Excitation) (+ SE-ResNet18C/D) - arXiv:1709.01507
+
 """
 
 import torch
@@ -177,11 +181,106 @@ class ResNet18C(nn.Module):
         return x
 
 
+class SELayer(nn.Module):
+
+    def __init__(self, c, r):
+        super().__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+            nn.Linear(c, c // r, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c // r, c, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.shape
+        y = self.squeeze(x).view(b, c)
+        y = self.excitation(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class SEResidualBlock(nn.Module): 
+    def __init__(self, inchannel, outchannel, stride=1, r=16): 
+        
+        super().__init__()
+        
+        self.left = nn.Sequential(nn.Conv2d(inchannel, outchannel, kernel_size=3, 
+                                         stride=stride, padding=1, bias=False), 
+                                  nn.BatchNorm2d(outchannel), 
+                                  nn.ReLU(inplace=True), 
+                                  nn.Conv2d(outchannel, outchannel, kernel_size=3, 
+                                         stride=1, padding=1, bias=False), 
+                                  nn.BatchNorm2d(outchannel),
+                                  SELayer(outchannel, r),
+                                  ) 
+        
+        self.shortcut = nn.Sequential() 
+        
+        if stride != 1 or inchannel != outchannel: 
+            
+            self.shortcut = nn.Sequential(nn.Conv2d(inchannel, outchannel, 
+                                                 kernel_size=1, stride=stride, 
+                                                 padding = 0, bias=False), 
+                                          nn.BatchNorm2d(outchannel) ) 
+            
+    def forward(self, x): 
+        
+        out = self.left(x) 
+        out += self.shortcut(x) 
+        out = F.relu(out) 
+        return out
+
+
+class SEResidualBlockTweaked(nn.Module): 
+    def __init__(self, inchannel, outchannel, stride=1, r=16): 
+        
+        super().__init__()
+        
+        self.left = nn.Sequential(nn.Conv2d(inchannel, outchannel, kernel_size=3, 
+                                         stride=stride, padding=1, bias=False), 
+                                  nn.BatchNorm2d(outchannel), 
+                                  nn.ReLU(inplace=True), 
+                                  nn.Conv2d(outchannel, outchannel, kernel_size=3, 
+                                         stride=1, padding=1, bias=False), 
+                                  nn.BatchNorm2d(outchannel),
+                                  SELayer(outchannel, r),
+                                  ) 
+        
+        self.shortcut = nn.Sequential() 
+        
+        if stride != 1 or inchannel != outchannel: 
+            
+            self.shortcut = nn.Sequential(nn.AvgPool2d(kernel_size=2, stride=2),
+                                          nn.Conv2d(inchannel, outchannel, 
+                                                    kernel_size=1, stride=1, 
+                                                    padding = 0, bias=False), 
+                                          nn.BatchNorm2d(outchannel)) 
+            
+    def forward(self, x): 
+        
+        out = self.left(x) 
+        out += self.shortcut(x) 
+        out = F.relu(out) 
+        return out
+
+
+class SE_Resnet18(ResNet18):
+    
+    def __init__(self):
+        super().__init__(ResidualBlock=SEResidualBlock)
+
+
 class ResNet18D(ResNet18):
 
     def __init__(self):
         super().__init__(ResidualBlock=ResidualBlockTweaked)
 
+
+class SE_ResNet18D(ResNet18):
+
+    def __init__(self):
+        super().__init__(ResidualBlock=SEResidualBlockTweaked)
 
 
 if __name__ == "__main__":
